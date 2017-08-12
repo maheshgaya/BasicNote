aa6a99416d683417c4d31322f9a2ad046b17d575
Mahesh Gaya <mmaheshgaya@gmail.com>
2017-08-12 00:00:46 -0500

    initial commit

---
+++ app/src/main/java/com/maheshgaya/android/basicnote/ui/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.maheshgaya.android.basicnote.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.maheshgaya.android.basicnote.R
+
+/**
+ * Created by Mahesh Gaya on 8/11/17.
+ */
+class AuthActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
