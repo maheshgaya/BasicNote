7f1cebc01dfb2a21d45568cb2ba9b0d28be49eae
Mahesh Gaya <mmaheshgaya@gmail.com>
2017-08-30 21:20:19 -0500

    Added logic for handle span
    - handles span for italic and bold

--- app/src/main/java/com/maheshgaya/android/basicnote/widget/NoteEditText.kt
+++ app/src/main/java/com/maheshgaya/android/basicnote/widget/NoteEditText.kt
@@ -3,7 +3,9 @@ package com.maheshgaya.android.basicnote.widget
 import android.content.Context
 import android.graphics.Typeface
 import android.text.Editable
+import android.text.Spannable
 import android.text.TextWatcher
+import android.text.style.StyleSpan
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.EditText
@@ -73,7 +75,7 @@ class NoteEditText : EditText, NoteEditorMenu.Callback {
     override fun onSelectionChanged(selStart: Int, selEnd: Int) {
         super.onSelectionChanged(selStart, selEnd)
         val html = text.toHtml()
-        Log.d(TAG, "onSelectionChanged=\n$html\nselStart=$selStart\tselEnd=$selEnd" )
+        Log.d(TAG, "onSelectionChanged=\n$html selStart=$selStart\tselEnd=$selEnd" )
     }
 
     private fun handleUnderlineStyle(selStart: Int, selEnd: Int){
@@ -81,6 +83,45 @@ class NoteEditText : EditText, NoteEditorMenu.Callback {
     }
 
     private fun handleSpannableStyle(selStart: Int, selEnd: Int, type:Int){
+        var exist = false
+        var styleSpans = text.getSpans(selStart, selEnd, StyleSpan::class.java)
+        var styleStart = -1
+        var styleEnd = -1
+        for (span in styleSpans){
+           if (span.style == type){
+               if (text.getSpanStart(span) < selStart){
+                   styleStart = text.getSpanStart(span)
+               }
+               if (text.getSpanEnd(span) > selEnd){
+                   styleEnd = text.getSpanEnd(span)
+               }
+               text.removeSpan(span)
+               exist = true
+           }
+        }
+
+        if (styleStart > -1){
+            text.setSpan(StyleSpan(type), styleStart, selStart, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        if (styleEnd> -1){
+            text.setSpan(StyleSpan(type), selEnd, styleEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        if (!exist){
+            text.setSpan(StyleSpan(type), selStart, selEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        } else {
+            when (type){
+                Typeface.BOLD -> {
+                    mNoteMenu?.boldCheckedButton?.isChecked = false
+                }
+                Typeface.ITALIC -> {
+                    mNoteMenu?.italicCheckedButton?.isChecked = false
+                }
+            }
+        }
+
+
 
     }
 
@@ -116,12 +157,18 @@ class NoteEditText : EditText, NoteEditorMenu.Callback {
 
     inner class NoteTextWatcher:TextWatcher{
         override fun afterTextChanged(editable: Editable?) {
+            clearComposingText()
+            Log.d(TAG, "afterTextChanged= " + editable?.toHtml())
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            clearComposingText()
+            Log.d(TAG, "beforeTextChanged= $s"  )
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            clearComposingText()
+            Log.d(TAG, "onTextChanged= $s")
         }
 
     }
