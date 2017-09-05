64bb47191252fb5b4419d9704b67216d28a68170
Mahesh Gaya <mmaheshgaya@gmail.com>
2017-09-04 20:56:17 -0500

    feat: added main app

---
+++ app/src/main/java/com/maheshgaya/android/basicnote/MainApplication.kt
@@ -0,0 +1,19 @@
+package com.maheshgaya.android.basicnote
+
+import android.app.Application
+import com.google.firebase.database.FirebaseDatabase
+
+/**
+ * Created by Mahesh Gaya on 8/12/17.
+ */
+class MainApplication:Application() {
+    /**
+     * Keep Firebase persistence for offline transaction
+     */
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+    }
+
+}
